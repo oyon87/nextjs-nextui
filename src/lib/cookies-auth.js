dadafe41a2e076fe095b3d8087f00b611373d793
Oyon87 <oyon87@yahoo.com>
2023-12-18 00:03:15 +0700

    domain cookies

--- src/lib/cookies-auth.js
+++ src/lib/cookies-auth.js
@@ -3,7 +3,11 @@
 import { cookies } from 'next/headers';
 
 const setAuthCookies = async (auth) => {
-  await cookies().set('auth', JSON.stringify(auth), { httpOnly: true, secure: true });
+  await cookies().set('auth', JSON.stringify(auth), {
+    httpOnly: true,
+    secure: true,
+    domain: '.netlify.app',
+  });
 };
 
 export { setAuthCookies };
