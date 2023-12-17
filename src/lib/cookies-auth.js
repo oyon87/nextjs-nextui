33723683507c066716f7050996add85a89f1fc21
Oyon87 <oyon87@yahoo.com>
2023-12-17 21:21:55 +0700

    cookie fix

--- src/lib/cookies-auth.js
+++ src/lib/cookies-auth.js
@@ -3,7 +3,7 @@
 import { cookies } from 'next/headers';
 
 const setAuthCookies = async (auth) => {
-  await cookies().set('auth', JSON.stringify(auth));
+  await cookies().set('auth', JSON.stringify(auth), { httpOnly: true, secure: true });
 };
 
 export { setAuthCookies };
