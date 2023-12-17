09f9f89a22ed1ff34c36bc1485e8a9f57128ec9e
Oyon87 <oyon87@yahoo.com>
2023-12-18 00:19:11 +0700

    same site cookie none

--- src/lib/cookies-auth.js
+++ src/lib/cookies-auth.js
@@ -5,7 +5,8 @@ import { cookies } from 'next/headers';
 const setAuthCookies = async (auth) => {
   await cookies().set('auth', JSON.stringify(auth), {
     httpOnly: true,
-    domain: 'yon-nextjs.netlify.app',
+    secure: true,
+    sameSite: 'None',
   });
 };
 
