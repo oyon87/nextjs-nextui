6e9d7024a9cf69fb4a0e2c26e3a18e9c65517ebd
Oyon87 <oyon87@yahoo.com>
2023-12-18 23:18:52 +0700

    cookies-next plugin

--- src/lib/cookies-auth.js
+++ src/lib/cookies-auth.js
@@ -1,13 +1,8 @@
-'use server';
+// import { cookies } from 'next/headers';
+import { setCookie } from 'cookies-next';
 
-import { cookies } from 'next/headers';
-
-const setAuthCookies = async (auth) => {
-  await cookies().set('auth', JSON.stringify(auth), {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-  });
+const setAuthCookies = (auth) => {
+  setCookie('auth', JSON.stringify(auth));
 };
 
 export { setAuthCookies };
