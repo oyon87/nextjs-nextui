271dccd82451c4c458f9023676f4bec7a64a6d64
Oyon87 <oyon87@yahoo.com>
2023-12-25 13:56:22 +0700

    remove hardcoded login user from context

--- src/contexts/login-context.js
+++ src/contexts/login-context.js
@@ -5,13 +5,7 @@ import { createContext, useContext, useState } from "react";
 const LoginContext = createContext(null);
 
 function LoginContextProvider({ children }) {
-  const [login, setLogin] = useState({
-    email: "test@gmail.com",
-    firstName: "Yon",
-    lastName: "Andreas",
-    username: "oyon87",
-    image: "https://i.pravatar.cc/150?u=a042581f4e29026704d",
-  });
+  const [login, setLogin] = useState({});
 
   return <LoginContext.Provider value={{ login, setLogin }}>{children}</LoginContext.Provider>;
 }
